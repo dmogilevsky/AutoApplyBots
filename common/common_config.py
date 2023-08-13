989c5b6ffb47fe61b41516c86c1d59dd8c50ac12
Daniel Mogilevsky <mogilevsky.daniel@gmail.com>
2023-08-12 20:02:49 -0400

    Attempted creating common utils and config modules

---
+++ common/common_config.py
@@ -0,0 +1,15 @@
+chromeProfilePath = r"/home/ongun/.config/google-chrome/Profile 3"
+
+# Should the bot wait for the user to fill in missing information on a job application or move on
+wait_for_user_input = False
+
+# When the bot fails to apply to an application, should it keep it open in a seperate tab
+keep_failed_applications_open = True
+
+# Run the browser in the background
+headless = False
+
+keywords = ["software engineer", "golang", "python", "java", "sql", "c#", ".net"]
+blacklisted_words = ["architect", "principal", "manager", "product owner", "front end", "lead"]
+blacklistCompanies = ["Jobot", "Cybercoders", "Braintrust", "GE", "GE Healthcare", "ConnectWise"]
+global_email = "mogilevsky.daniel@gmail.com"
